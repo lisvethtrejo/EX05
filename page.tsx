fedd036e0c4b9210729b23d50818826c10b5e166
lisvethtrejo <ltrejo@usf.edu>
2025-04-29 16:40:36 -0400

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,24 +1,30 @@
-import { JSX } from "react";
+"use client";
+import { ClientRequest } from "http";
+import type { NextPage } from "next";
+import React, { useState, useEffect } from "react";
 
-export default async function ServerComponentUserList():Promise<JSX.Element> {
-    const url = "https://www.usemodernfullstack.dev/api/v1/users";
-    let data: ResponseType[] | [] = [];
-    let names: ResponseType[] | [];
+const PageComponentWeather: NextPage = () => {
 
-    try {
-        const response = await fetch(url, { cache: "force-cache" })
-        data = (await response.json()) as ResponseType[];
-    } catch (err) {
-        throw new Error("Failed to fetch data");
+    interface WeatherProps {
+        weather: string;
     }
-    names = data.map((item) => {
-        return { id: item.id, name: item.name };
-    });
-  return (
-    <ul>
-        {names.map((item) => (
-            <li key={item.id}>{item.id}.{item.name}</li>
-        ))}
-    </ul>
-  );
-}
+
+    const WeatherComponent = (props: WeatherProps) => {
+        
+        const [count, setCount] = useState(0);
+        useEffect(() => {
+            setCount(1);
+        }, []);
+
+        return (
+            <h1 onClick={() => setCount(count + 1)}>
+                The weather is { props.weather },
+                and the counter shows { count }
+            </h1>
+        );
+    };
+
+    return (<WeatherComponent weather="sunny" />);
+};
+
+export default PageComponentWeather;
