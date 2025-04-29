f84cc5a21aac66311cf63f799a703e949c9d3aa4
lisvethtrejo <ltrejo@usf.edu>
2025-04-29 16:37:15 -0400

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,6 +1,24 @@
-import type { NextPage } from "next";
+import { JSX } from "react";
 
-const Hello: NextPage = () => {
-    return (<>Hello World!</>);
+export default async function ServerComponentUserList():Promise<JSX.Element> {
+    const url = "https://www.usemodernfullstack.dev/api/v1/users";
+    let data: ResponseType[] | [] = [];
+    let names: ResponseType[] | [];
+
+    try {
+        const response = await fetch(url, { cache: "force-cache" })
+        data = (await response.json()) as ResponseType[];
+    } catch (err) {
+        throw new Error("Failed to fetch data");
+    }
+    names = data.map((item) => {
+        return { id: item.id, name: item.name };
+    });
+  return (
+    <ul>
+        {names.map((item) => (
+            <li key={item.id}>{item.id}.{item.name}</li>
+        ))}
+    </ul>
+  );
 }
-export default Hello;
